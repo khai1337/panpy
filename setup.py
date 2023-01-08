a27f20114380f4882f596e702ad4f80dea8380bc
khai <alight2rich@proton.me>
2023-01-08 13:28:18 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="panelyz.com",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    license="MIT License",
+    description="A panelyz.com api wrapper in python",
+    keywords="panelyz, api wrapper, python3",
+    url="https://github.com/khai1337/Panelyz-api-wrapper",
+    author="khai1337",
+    author_email="alight2rich@proton.me",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ]
+)

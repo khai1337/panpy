c6f3ec15028430220cc902803706eca766783564
khai <alight2rich@proton.me>
2023-01-10 19:53:47 +0100

    Update panel.py

--- panelyz/panel.py
+++ panelyz/panel.py
@@ -1,145 +1,143 @@
 import requests, typing
 
-
 class Panel:
-    global api_key, base_url
-    api_key = "258dffa2acfd0761699f01d030c57aa8"
-    base_url = "https://panelyz.com/api/v2"
+
+    def __init__(self):
+        self.api_key = "your own api key"
+        self.base_url = "https://panelyz.com/api/v2"
+
+    #---    get user balance   ---#
+    def getBalance(self):
+        try:
+
+            p = {
+                "key": self.api_key,
+                "action": "balance"
+            }
+
+            re = requests.post(self.base_url, params=p)
+
+            if re.status_code == 200:
+                print(re.text)
+
+        except Exception as e:
+            print(e)
 
 
 
-#---    get user balance   ---#
-def getBalance():
-    try:
+    #---    add order   ---#
+    def addOrder(self, service: int, quantity: int, link: str):
+        try:
+            p = {
+                "key": self.api_key,
+                "action": "add",
+                "service": service,
+                "link": link,
+                "quantity": quantity
+            }
 
-        p = {
-            "key": api_key,
-            "action": "balance"
-        }
+            re = requests.post(self.base_url, params=p)
 
-        re = requests.post(base_url, params=p)
+            if re.status_code == 200:
+                print(re.text)
 
-        if re.status_code == 200:
-            print(re.text)
-
-    except Exception as e:
-        print(e)
-
-
-
-#---    add order   ---#
-def addOrder(service: int, quantity: int, link: str):
-    try:
-        p = {
-            "key": api_key,
-            "action": "add",
-            "service": service,
-            "link": link,
-            "quantity": quantity
-        }
-
-        re = requests.post(base_url, params=p)
-
-        if re.status_code == 200:
-            print(re.text)
-
-    except Exception as e:
-        print(e)
+        except Exception as e:
+            print(e)
 
 
 
-#---    get order status   ---#
-def getOrderStat(order: int):
-    try:
-        p = {
-            "key": api_key,
-            "action": "status",
-            "order": order
-        }
+    #---    get order status   ---#
+    def getOrderStat(self, order: int):
+        try:
+            p = {
+                "key": self.api_key,
+                "action": "status",
+                "order": order
+            }
 
-        re = requests.post(base_url, params=p)
+            re = requests.post(self.base_url, params=p)
 
-        if re.status_code == 200:
-            print(re.text)
+            if re.status_code == 200:
+                print(re.text)
 
-    except Exception as e:
-        print(e)
-
-
-
-#---    get multiple order status   ---#
-def getMultOrderStat(orders: typing.Union[list, int]):
-    try:
-        p = {
-            "key": api_key,
-            "action": "status",
-        }
-
-        if type(orders) == int:
-            p["order"] = orders
-        else:
-            p["orders"] = orders
-
-        re = requests.post(base_url, params=p)
-
-        if re.status_code == 200:
-            print(re.text)
-
-    except Exception as e:
-        print(e)
+        except Exception as e:
+            print(e)
 
 
 
-#---    gets services   ---#
-def getServices():
-    try:
-        p = {
-            "key": api_key,
-            "action": "services"
-        }
+    #---    get multiple order status   ---#
+    def getMultOrderStat(self, orders: typing.Union[list, int]):
+        try:
+            p = {
+                "key": self.api_key,
+                "action": "status",
+            }
 
-        re = requests.post(base_url, params=p)
+            if type(orders) == int:
+                p["order"] = orders
+            else:
+                p["orders"] = orders
 
-        if re.status_code == 200:
-            print(re.text)
+            re = requests.post(self.base_url, params=p)
 
-    except Exception as e:
-        print(e)
+            if re.status_code == 200:
+                print(re.text)
 
-
-
-#---    create refill   ---#
-def createRefill(order: int):
-    try:
-        p = {
-            "key": api_key,
-            "action": "refill",
-            "order": order
-        }
-
-        re = requests.post(base_url, params=p)
-
-        if re.status_code == 200:
-            print(re.text)
-
-    except Exception as e:
-        print(e)
+        except Exception as e:
+            print(e)
 
 
 
-#---    get refill status   ---#
-def getRefillStat(refill_id: int):
-    try:
-        p = {
-            "key": api_key,
-            "action": "refill_status",
-            "refill": refill_id
-        }
+    #---    gets services   ---#
+    def getServices(self):
+        try:
+            p = {
+                "key": self.api_key,
+                "action": "services"
+            }
 
-        re = requests.post(base_url, params=p)
+            re = requests.post(self.base_url, params=p)
 
-        if re.status_code == 200:
-            print(re.text)
+            if re.status_code == 200:
+                print(re.text)
 
-    except Exception as e:
-        print(e)
+        except Exception as e:
+            print(e)
+
+
+
+    #---    create refill   ---#
+    def createRefill(self, order: int):
+        try:
+            p = {
+                "key": self.api_key,
+                "action": "refill",
+                "order": order
+            }
+
+            re = requests.post(self.base_url, params=p)
+
+            if re.status_code == 200:
+                print(re.text)
+
+        except Exception as e:
+            print(e)
+
+
+
+    #---    get refill status   ---#
+    def getRefillStat(self, refill_id: int):
+            try:
+                p = {
+                    "key": self.api_key,
+                    "action": "refill_status",
+                    "refill": refill_id
+                }
+
+                re = requests.post(self.base_url, params=p)
+
+                if re.status_code == 200:
+                    print(re.text)
+
+            except Exception as e:
+                print(e)
